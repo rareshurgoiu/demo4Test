e4ff040e9bab4363c609c20f2c4f354c26ed677b
Rares Hurgoiu <Rares.Hurgoiu@msg.group>
2022-07-11 14:36:45 +0200

    Add message2

--- src/com/msg/demo/Main.java
+++ src/com/msg/demo/Main.java
@@ -3,6 +3,7 @@ package com.msg.demo;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Buna Demo4 Test!");
+	System.out.println("Buna Demo4 Test1!");
+        System.out.println("Buna Demo4 Test2!");
     }
 }
